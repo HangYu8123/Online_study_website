ef80ff02eb9ff5c021ee09dad4d14c99c8e7fd4a
hang <yuhang8123@outlook.com>
2023-08-31 12:23:13 -0400

    videos

--- Phase_2/progress/static/video_cliping.py
+++ Phase_2/progress/static/video_cliping.py
@@ -2,53 +2,11 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 from moviepy.video import fx
 
 # Load video
-clip = VideoFileClip("webpage/static/marked_moving.avi")
-clip = clip.subclip(50, 295)
+clip = VideoFileClip("Phase_2/progress/static/imperfect_demo.avi")
+clip = clip.subclip(5, 920).without_audio()
 # Determine clip duration and the duration of each segment
 total_duration = clip.duration
-segment_duration = total_duration / 10
-
-clips = []
-
-for i in range(10):
-    start_time = i * segment_duration
-    end_time = (i + 1) * segment_duration
-    
-    # Cut video into a clip
-    cut_clip = clip.subclip(start_time, end_time)
-    
-    # Speed up the clip
-    speed_up_factor = 2  # 2x speed
-    fast_clip = cut_clip.fx(fx.all.speedx, speed_up_factor)
-    fast_clip.write_videofile("webpage/static/episode" + str(0)+"step"+str(i)+".mp4")
-
-# Load video
-clip = VideoFileClip("webpage/static/marked_pouring.avi")
-clip = clip.subclip(40, 640)
-# Determine clip duration and the duration of each segment
-total_duration = clip.duration
-segment_duration = total_duration / 10
-
-clips = []
-
-for i in range(10):
-    start_time = i * segment_duration
-    end_time = (i + 1) * segment_duration
-    
-    # Cut video into a clip
-    cut_clip = clip.subclip(start_time, end_time)
-    
-    # Speed up the clip
-    speed_up_factor = 5  # 2x speed
-    fast_clip = cut_clip.fx(fx.all.speedx, speed_up_factor)
-    fast_clip.write_videofile("webpage/static/episode" + str(1)+"step"+str(i)+".mp4")
-
-# Load video
-clip = VideoFileClip("webpage/static/marked_spining.avi")
-clip = clip.subclip(30, 750)
-# Determine clip duration and the duration of each segment
-total_duration = clip.duration
-segment_duration = total_duration / 10
+segment_duration = total_duration / 15
 
 clips = []
 
@@ -62,7 +20,49 @@ for i in range(10):
     # Speed up the clip
     speed_up_factor = 6  # 2x speed
     fast_clip = cut_clip.fx(fx.all.speedx, speed_up_factor)
-    fast_clip.write_videofile("webpage/static/episode" + str(2)+"step"+str(i)+".mp4")
+    fast_clip.write_videofile("Phase_2/progress/static/episode" + str(1)+"step"+str(i)+".mp4")
+
+# # Load video
+# clip = VideoFileClip("webpage/static/marked_pouring.avi")
+# clip = clip.subclip(40, 640)
+# # Determine clip duration and the duration of each segment
+# total_duration = clip.duration
+# segment_duration = total_duration / 10
+
+# clips = []
+
+# for i in range(10):
+#     start_time = i * segment_duration
+#     end_time = (i + 1) * segment_duration
+    
+#     # Cut video into a clip
+#     cut_clip = clip.subclip(start_time, end_time)
+    
+#     # Speed up the clip
+#     speed_up_factor = 5  # 2x speed
+#     fast_clip = cut_clip.fx(fx.all.speedx, speed_up_factor)
+#     fast_clip.write_videofile("webpage/static/episode" + str(1)+"step"+str(i)+".mp4")
+
+# # Load video
+# clip = VideoFileClip("webpage/static/marked_spining.avi")
+# clip = clip.subclip(30, 750)
+# # Determine clip duration and the duration of each segment
+# total_duration = clip.duration
+# segment_duration = total_duration / 10
+
+# clips = []
+
+# for i in range(10):
+#     start_time = i * segment_duration
+#     end_time = (i + 1) * segment_duration
+    
+#     # Cut video into a clip
+#     cut_clip = clip.subclip(start_time, end_time)
+    
+#     # Speed up the clip
+#     speed_up_factor = 6  # 2x speed
+#     fast_clip = cut_clip.fx(fx.all.speedx, speed_up_factor)
+#     fast_clip.write_videofile("webpage/static/episode" + str(2)+"step"+str(i)+".mp4")
 
 
 
