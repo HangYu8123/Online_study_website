462e3e984f974bb64c9a466e83fed0325a89d539
Hang <yuhang8123@outlook.com>
2023-09-19 08:52:01 -0400

    haha

--- Phase_2/progress/main.py
+++ Phase_2/progress/main.py
@@ -11,7 +11,7 @@ import os
 
 
 letters = string.ascii_letters
-eps_len_list = [15, 15, 15, 15, 15]
+eps_len_list = [15, 15, 15, 18, 15]
 app = Flask(__name__)
 # Setup the secret key and the environment
 app.config.update(SECRET_KEY='osd(99092=36&462134kjKDhuIS_d23',
